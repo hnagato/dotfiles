f53281e8f01e6c39901bb09fe733e3934eb4522e
hnagato <134620+hnagato@users.noreply.github.com>
2025-06-04 18:03:23 +0900

    refactor: :recycle: tidy

--- setup.rb
+++ setup.rb
@@ -1,62 +1,85 @@
 #!/usr/bin/env ruby
 
-require 'pathname'
 require 'fileutils'
+require 'pathname'
 require 'optparse'
 
 include FileUtils::Verbose
 
-opt = OptionParser.new
-opt.on('-d', '--dry-run') {|v| include FileUtils::DryRun }
-opt.parse!(ARGV)
+def error(msg)
+  warn "ERROR: #{msg}"
+  exit 1
+end
 
 class String
-  def expand
+  def to_path
     Pathname.new(self).expand_path
   end
-end
+end  
 
 class Pathname
   def / other
-    (self + other).expand_path
-  end
-
-  def to_bak
-    (self.realpath.to_s + '.org').expand
-  end
-end
-
-def symlink src, dest
-  # backup or remove
-  if dest.exist?
-    if dest.symlink?
-      rm dest
+    case other
+    when Pathname
+      (self + other.basename.to_s).expand_path
     else
-      mv dest, dest.to_bak
+      (self + other.to_s).expand_path
     end
   end
-  ln_sf src.expand_path, dest
+  
+  def basename?(name)
+    case name
+    when Regexp
+      basename.to_s =~ name
+    else
+      basename.to_s == name.to_s
+    end
+  end
+  
+  def safe_symlink(target)
+    return if symlink? && readlink == target
+    rmtree if exist?
+    make_symlink(target)
+  end
 end
 
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+  opts.on('-t', '--test', 'Setup in /tmp instead of $HOME') { options[:test] = true }
+  opts.on('-h', '--help', 'Show this help') { puts opts; exit }
+end.parse!
 
-HOME = '~'.expand
-BASE = File.dirname(__FILE__).expand
+target = (options[:test] ? "/tmp/#{ENV['USER']}" : ENV['HOME']).to_path
+dotfiles = __FILE__.to_path.dirname
 
-cd BASE do
-  Pathname.glob('.*') do |dotfile|
-    next if dotfile.to_s =~ /^\.{1,2}$/
-    next if dotfile.to_s =~ /^\.(config|DS_Store|git(ignore|modules)?)$/
-    next if dotfile.symlink?
+mkdir_p target
 
-    symlink BASE/dotfile, HOME/dotfile
-  end
+# Link dotfiles (skip special cases)
+dotfiles.glob('.*').select(&:file?).each do |file|
+  next if file.basename?(/^\.(git.*|config)$/)
 
-  Pathname.glob('.config/**/*') do |config|
-    next if config.symlink? or config.directory?
+  link = target/file
+  link.safe_symlink(file)
+end
 
-    dir = (HOME/config).parent
-    dir.mkpath unless dir.exist?
+# Link .config files (skip fish/nvim)
+config_dir = target/'.config'
+mkdir_p config_dir
 
-    symlink BASE/config, HOME/config
-  end
+(dotfiles/'.config').glob('*') do |file|
+  next if file.basename?(/^(fish|nvim)$/)
+  
+  link = config_dir/file
+  link.safe_symlink(file)
+end
+
+# Setup fish
+fish_dir = target/'.config/fish'
+functions_dir = fish_dir/'functions'
+[fish_dir, functions_dir].each(&method(:mkdir_p))
+
+(dotfiles/'.config/fish').glob('*').select(&:file?).each do |file|
+  link = file.basename?('fish_greeting.fish') ? functions_dir/file : fish_dir/file
+  link.safe_symlink(file)
 end
