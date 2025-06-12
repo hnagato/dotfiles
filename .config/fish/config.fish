131646efb4c94b7e3375ad6533d684bd1f355819
hnagato <134620+hnagato@users.noreply.github.com>
2025-06-13 00:49:55 +0900

    chore: :hammer: update abbreviations and enhance local variable scoping

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -63,7 +63,8 @@ alias vi='nvim'
 abbr -a ... cd ../..
 abbr -a .... cd ../../../
 abbr -a ll ls -lhavGF
-abbr -a e code
+abbr -a e cursor
+abbr -a c code
 abbr -a i idea
 
 abbr -a gs  git status -sb
@@ -81,7 +82,6 @@ if type -q eza
   abbr -a lt eza -la --icons --git --git-ignore -T --ignore-glob .git --level=2
   abbr -a la eza -la --icons --color=never
   abbr -a ls eza --icons --git
-  set -gx EZA_COLORS "uu=38;5;249:un=38;5;241:gu=38;5;245:gn=38;5;241:da=38;5;245:sn=38;5;7:sb=38;5;7:ur=38;5;3;1:uw=38;5;5;1:ux=38;5;1;1:ue=38;5;1;1:gr=38;5;249:gw=38;5;249:gx=38;5;249:tr=38;5;249:tw=38;5;249:tx=38;5;249:fi=38;5;248:di=38;5;253:ex=38;5;1:xa=38;5;12:*.png=38;5;4:*.jpg=38;5;4:*.gif=38;5;4"
 end
 
 function psg
@@ -89,7 +89,7 @@ function psg
 end
 
 function push-line
-  set cl (commandline)
+  set -l cl (commandline)
   commandline -f repaint
   if test -n (string join $cl)
     set -g fish_buffer_stack $cl
