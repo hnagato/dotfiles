60a45cb9e948feffd7e4caec65f10039f26d0d87
hnagato <134620+hnagato@users.noreply.github.com>
2025-07-09 23:11:15 +0900

    refactor: :recycle: refactor fish config for improved readability and organization

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,7 +16,7 @@ set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
 set -gx fzf_fd_opts --hidden --exclude=.git
 set -gx fzf_preview_dir_cmd eza -la --color=always --git --ignore-glob .git
 set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --height=90% --preview-window=wrap --marker="*"'
-set -gx FZF_TMUX_OPTS '-p'
+set -gx FZF_TMUX_OPTS -p
 set -gx fzf_history_time_format "%Y-%m-%d %H:%M:%S"
 
 # paths
@@ -24,74 +24,74 @@ fish_add_path -m $HOME/.local/bin
 fish_add_path $HOME/.rd/bin
 
 if status is-interactive
-  set HOMEBREW_HOME /opt/homebrew
-  if test -d $HOMEBREW_HOME/bin
-    eval ($HOMEBREW_HOME/bin/brew shellenv)
-    fish_add_path $HOMEBREW_HOME/bin $HOMEBREW_HOME/sbin
-    if test -d $HOMEBREW_HOME/opt/mysql@8.0
-      fish_add_path $HOMEBREW_HOME/opt/mysql@8.0/bin
+    set HOMEBREW_HOME /opt/homebrew
+    if test -d $HOMEBREW_HOME/bin
+        eval ($HOMEBREW_HOME/bin/brew shellenv)
+        fish_add_path $HOMEBREW_HOME/bin $HOMEBREW_HOME/sbin
+        if test -d $HOMEBREW_HOME/opt/mysql@8.0
+            fish_add_path $HOMEBREW_HOME/opt/mysql@8.0/bin
+        end
     end
-  end
-  if type -q rdctl; and rdctl shell true &>/dev/null
-    set -gx DOCKER_HOST "unix://$HOME/.rd/docker.sock"
-    set -gx TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE "/var/run/docker.sock"
-    set -gx TESTCONTAINERS_HOST_OVERRIDE (rdctl shell ip a show vznat | awk '/inet / {sub("/.*",""); print $2}')
-  end
+    if type -q rdctl; and rdctl shell true &>/dev/null
+        set -gx DOCKER_HOST "unix://$HOME/.rd/docker.sock"
+        set -gx TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE "/var/run/docker.sock"
+        set -gx TESTCONTAINERS_HOST_OVERRIDE (rdctl shell ip a show vznat | awk '/inet / {sub("/.*",""); print $2}')
+    end
 
-  if type -q starship
-    starship init fish | source
-  end
+    if type -q starship
+        starship init fish | source
+    end
 
-  if type -q mise
-    mise activate fish | source
-  end
+    if type -q mise
+        mise activate fish | source
+    end
 
-  if type -q pyenv
-    pyenv init - | source
-  end
+    if type -q pyenv
+        pyenv init - | source
+    end
 end
 
 if test -d $HOME/.cargo/bin
-  fish_add_path $HOME/.cargo/bin
+    fish_add_path $HOME/.cargo/bin
 end
 
 # aliases & abbrs
-alias t='tmux attach-session -d || tmux new' 
+alias t='tmux attach-session -d || tmux new'
 alias tn='tmux new-session'
 alias vi='nvim'
 
-abbr -a ...   cd ../..
-abbr -a ....  cd ../../../
-abbr -a ll    ls -lhavGF
-abbr -a e     cursor
-abbr -a c     code
-abbr -a i     idea
-abbr -a cc    claude
-abbr -a gs    git status -sb
-abbr -a gco   git checkout
-abbr -a gfa   git fetch --all
-abbr -a gl    git l
-abbr -a ga    git add
-abbr -a gc    git czg ai emoji
-abbr -a gb    git branch
-abbr -a gd    git diff -ubw
-abbr -a gp    git pull
+abbr -a ... cd ../..
+abbr -a .... cd ../../../
+abbr -a ll ls -lhavGF
+abbr -a e cursor
+abbr -a c code
+abbr -a i idea
+abbr -a cc claude
+abbr -a gs git status -sb
+abbr -a gco git checkout
+abbr -a gfa git fetch --all
+abbr -a gl git l
+abbr -a ga git add
+abbr -a gc git czg ai emoji
+abbr -a gb git branch
+abbr -a gd git diff -ubw
+abbr -a gp git pull
 
 if type -q eza
-  abbr -a lf eza -la --icons --git --ignore-glob .git
-  abbr -a lt eza -la --icons --git --git-ignore -T --ignore-glob .git --level=2
-  abbr -a la eza -la --icons --color=never
-  abbr -a ls eza --icons --git
+    abbr -a lf eza -la --icons --git --ignore-glob .git
+    abbr -a lt eza -la --icons --git --git-ignore -T --ignore-glob .git --level=2
+    abbr -a la eza -la --icons --color=never
+    abbr -a ls eza --icons --git
 end
 
 function psg
-  ps -ef | grep -i $argv
+    ps -ef | grep -i $argv
 end
 
 if test -f "$HOME/.config/op/plugins.sh"
-  source "$HOME/.config/op/plugins.sh"
+    source "$HOME/.config/op/plugins.sh"
 end
 
 if test -f "$HOME/.claude/local/claude"
-  alias claude="$HOME/.claude/local/claude"
+    alias claude="$HOME/.claude/local/claude"
 end
--- .config/fish/functions/fzf_z_search.fish
+++ .config/fish/functions/fzf_z_search.fish
@@ -1,14 +1,14 @@
 function fzf_z_search
-  set -l query (commandline)
-  z -lr |
-      _fzf_wrapper --exit-0 --print0 --no-multi --query="$query" --prompt="z> " \
-          --preview-window="right,50%" \
-          --preview="_fzf_preview_file (echo -n {} | cut -c 12- | tr -d '\n')" |
-      cut -c 12- |
-      read -l recent
-  if [ $recent ]
-    cd $recent
-    commandline -r ''
-  end
-  commandline -f repaint
+    set -l query (commandline)
+    z -lr |
+        _fzf_wrapper --exit-0 --print0 --no-multi --query="$query" --prompt="z> " \
+            --preview-window="right,50%" \
+            --preview="_fzf_preview_file (echo -n {} | cut -c 12- | tr -d '\n')" |
+        cut -c 12- |
+        read -l recent
+    if [ $recent ]
+        cd $recent
+        commandline -r ''
+    end
+    commandline -f repaint
 end
