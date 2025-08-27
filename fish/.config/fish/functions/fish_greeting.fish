2e9f45d4be660745f432931ec75096baf71638b2
hnagato <134620+hnagato@users.noreply.github.com>
2025-08-28 02:21:28 +0900

    fix: Replace fish package symlinks with actual files
    - Remove problematic symlinks from fish/.config/fish/
    - Copy actual files from .config/fish/ to fish/.config/fish/
    - Resolve broken symlinks in test environment
    - Maintain .stow-local for functions directory control
    - Successful test: stow now creates correct file links instead of broken symlink chains

---
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -0,0 +1,8 @@
+function fish_greeting
+    if test -z "$FZF_PROJECTS_ROOT"
+        eval (op signin)
+
+        set -U FZF_PROJECTS_ROOT (op item get "shell-env" --format=json | jq -r '.fields[] | select(.label=="FZF_PROJECTS_ROOT").value')
+        set -U HOMEBREW_GITHUB_API_TOKEN (op item get "shell-env" --format=json | jq -r '.fields[] | select(.label=="HOMEBREW_GITHUB_API_TOKEN").value')
+    end
+end
---
+++ fish/.config/fish/functions/fzf_project.fish
@@ -0,0 +1,25 @@
+function fzf_project
+    set -f dir (eval echo -- $FZF_PROJECTS_ROOT)
+    set -f current_line (commandline)
+    set -f query ""
+    if not string match -q "* " "$current_line"
+        set -f words (string split " " "$current_line")
+        if test (count $words) -gt 0
+            set query $words[-1]
+        end
+    end
+    fd -d2 -td --base-directory $dir |
+        _fzf_wrapper --exit-0 --print0 --no-multi --query="$query" --prompt="project> " \
+            --preview-window="right,50%" \
+            --preview="_fzf_preview_file $dir/{}" |
+        read -l project_dir
+    if [ $project_dir ]
+        if test -n "$query"
+            set -f new_line (string replace -r "$query\$" "$dir/$project_dir" "$current_line")
+            commandline -r "$new_line"
+        else
+            commandline -i "$dir/$project_dir"
+        end
+    end
+    commandline -f repaint
+end
