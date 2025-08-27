a9d90a6a1b36379b5a862d07b892e72864724fed
hnagato <134620+hnagato@users.noreply.github.com>
2025-08-27 09:10:25 +0900

    refactor: clearer variable names and indentation

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,22 +1,22 @@
-function git --wraps=git --description 'Wrap git to run confirm-pam on --no-verify option'
-    set -l no_verify 0
-    set -l target_cmd 0
-    
-    for arg in $argv
-        switch $arg
-            case commit push
-                set target_cmd 1
-            case --no-verify -n
-                set no_verify 1
-        end
+function git --wraps=git --description "Wrap git to run confirm-pam on --no-verify option"
+  set -l has_no_verify 0
+  set -l is_commit_or_push 0
+
+  for arg in $argv
+    switch $arg
+    case commit push
+      set is_commit_or_push 1
+    case --no-verify -n
+      set has_no_verify 1
     end
-    
-    if test $no_verify -eq 1 -a $target_cmd -eq 1
-        if not confirm-pam "Dangerous --no-verify option. Continue?"
-            echo "❌ Authentication failed."
-            return 1
-        end
+  end
+
+  if test $has_no_verify -eq 1 -a $is_commit_or_push -eq 1
+    if not confirm-pam "Dangerous --no-verify option. Continue?"
+      echo "❌ Authentication failed."
+      return 1
     end
-    
-    command git $argv
+  end
+
+  command git $argv
 end
