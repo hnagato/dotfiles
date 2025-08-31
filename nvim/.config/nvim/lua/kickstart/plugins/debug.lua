253f9cebde257e7bab13ecbeb2e8dcad7a590414
hnagato <134620+hnagato@users.noreply.github.com>
2025-08-31 14:33:37 +0900

    refactor: Replace kickstart and custom init with a full custom Neovim config

--- nvim/.config/nvim/lua/kickstart/plugins/debug.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/debug.lua
@@ -124,7 +124,7 @@ return {
     -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
     -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
     -- local breakpoint_icons = vim.g.have_nerd_font
-    --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+    --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
     --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
     -- for type, icon in pairs(breakpoint_icons) do
     --   local tp = 'Dap' .. type
