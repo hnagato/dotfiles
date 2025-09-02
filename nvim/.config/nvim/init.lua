f74ab969813a1beeec9e18b003034e880243093c
hnagato <134620+hnagato@users.noreply.github.com>
2025-09-02 23:08:04 +0900

    refactor: lualine

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -333,7 +333,7 @@ require('lazy').setup({
     dependencies = 'rafamadriz/friendly-snippets',
     version = '*',
     opts = {
-      keymap = { 
+      keymap = {
         preset = 'enter',
         ['<C-e>'] = {},  -- Disable <C-e> for copilot integration
       },
@@ -471,16 +471,75 @@ require('lazy').setup({
     opts = {
       options = {
         theme = "auto",
-        component_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "│" },
         section_separators = { left = '', right = '' },
       },
       sections = {
         lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
         lualine_b = { 'filename', 'branch' },
         lualine_c = {
-          '%=', --[[ add your center compoentnts here in place of this comment ]]
+          '%=',
+          {
+            'diagnostics',
+            sources = { 'nvim_lsp' },
+            sections = { 'error', 'warn', 'info', 'hint' },
+            symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+            colored = true,
+            update_in_insert = false,
+            diagnostics_color = {
+              error = { fg = '#c4746e' }, -- dragonRed
+              warn  = { fg = '#c4b28a' }, -- dragonYellow
+              info  = { fg = '#8ba4b0' }, -- dragonBlue2
+              hint  = { fg = '#8992a7' }, -- dragonViolet
+            },
+          },
+          {
+            function()
+              local clients = vim.lsp.get_clients()
+              if next(clients) == nil then
+                return ''
+              end
+              local names = {}
+              for _, client in ipairs(clients) do
+                table.insert(names, client.name)
+              end
+              return ' LSP: ' .. table.concat(names, ', ')
+            end,
+            color = { fg = '#8ba4b0' }, -- dragonBlue2 from kanagawa-dragon
+            cond = function()
+              return next(vim.lsp.get_clients()) ~= nil
+            end,
+          },
+          {
+            'searchcount',
+            maxcount = 999,
+            timeout = 500,
+          },
         },
-        lualine_x = {},
+        lualine_x = {
+          {
+            'diff',
+            colored = true,
+            symbols = { added = '+', modified = '~', removed = '-' },
+            diff_color = {
+              added    = { fg = '#87a987' }, -- dragonGreen
+              modified = { fg = '#c4b28a' }, -- dragonYellow
+              removed  = { fg = '#c4746e' }, -- dragonRed
+            },
+          },
+          {
+            'encoding',
+            show_bomb = false,
+          },
+          {
+            'fileformat',
+            symbols = {
+              unix = 'LF',
+              dos = 'CRLF',
+              mac = 'CR',
+            },
+          },
+        },
         lualine_y = { 'filetype', 'progress' },
         lualine_z = {
           { 'location', separator = { right = '' }, left_padding = 2 },
