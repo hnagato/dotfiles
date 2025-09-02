cf9c0b6c0a71c9cc7451dade2eb2c769260467c0
hnagato <134620+hnagato@users.noreply.github.com>
2025-09-02 22:54:10 +0900

    refactor: Remove setup-tools.sh and simplify setup.sh tool installation

--- setup.sh
+++ setup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
-TARGET="$HOME"
-if [ "$1" = "-t" ]; then
-  if [ -n "$2" ] && [ "${2:0:1}" != "-" ]; then
-    TARGET="$2"
-    shift 2
-  else
-    TARGET="/tmp/$USER"
-    shift
-  fi
-  mkdir -p "$TARGET"
-fi
 
-if [ $# -eq 0 ]; then
-  find . -print0 -maxdepth 1 -type d -name '[^.]*' -exec basename {} \; | xargs -0 stow -t "$TARGET"
-else
-  stow -t "$TARGET" "$@"
+for dir in */; do
+  stow -t "$HOME" "$(basename "$dir")"
+done
+
+command -v fish >/dev/null 2>&1 && fish -c 'curl -sL git.io/fisher | source && fisher update'
+
+mkdir -p "$HOME/.local/share/tmux/plugins"
+if [ ! -d "$HOME/.local/share/tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.local/share/tmux/plugins/tpm"
 fi
+"$HOME/.local/share/tmux/plugins/tpm/bin/install_plugins"
+
+[ -d "$HOME/.ssh" ] && chmod 700 "$HOME/.ssh" && find "$HOME/.ssh" -type f -exec chmod 600 {} \;
