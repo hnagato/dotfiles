dd4702d4baff719ac242bce06a0e3d77a205b606
hnagato <134620+hnagato@users.noreply.github.com>
2025-09-10 22:13:24 +0900

    feat: textlint

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,7 +1,2 @@
 function fish_greeting
-    if test -z "$FZF_PROJECTS_ROOT"
-        eval (op signin)
-
-        set -U FZF_PROJECTS_ROOT (op item get "shell-env" --format=json | jq -r '.fields[] | select(.label=="FZF_PROJECTS_ROOT").value')
-    end
 end
