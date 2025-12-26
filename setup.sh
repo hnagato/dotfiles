27c34d6d1ca1ef8a47d180c29bd363af1b06b3f3
hnagato <134620+hnagato@users.noreply.github.com>
2025-12-26 19:47:10 +0900

    refactor: stow -> bash

--- setup.sh
+++ setup.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 set -e
 
-for dir in */; do
-  stow -t "$HOME" "$(basename "$dir")"
+test_mode=0
+for arg in "$@"; do
+  if [ "$arg" = "-t" ]; then
+    test_mode=1
+  fi
 done
 
-command -v fish >/dev/null 2>&1 && fish -c 'curl -sL git.io/fisher | source && fisher update'
+./link-dotfiles.sh "$@"
 
-mkdir -p "$HOME/.local/share/tmux/plugins"
-if [ ! -d "$HOME/.local/share/tmux/plugins/tpm" ]; then
-  git clone https://github.com/tmux-plugins/tpm "$HOME/.local/share/tmux/plugins/tpm"
+if [ "$test_mode" -eq 0 ]; then
+  command -v fish >/dev/null 2>&1 && fish -c 'curl -sL git.io/fisher | source && fisher update'
+
+  mkdir -p "$HOME/.local/share/tmux/plugins"
+  if [ ! -d "$HOME/.local/share/tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.local/share/tmux/plugins/tpm"
+  fi
+  "$HOME/.local/share/tmux/plugins/tpm/bin/install_plugins"
 fi
-"$HOME/.local/share/tmux/plugins/tpm/bin/install_plugins"
 
-[ -d "$HOME/.ssh" ] && chmod 700 "$HOME/.ssh" && find "$HOME/.ssh/" -type f -exec chmod 600 {} \;
+target_root="$HOME"
+[ "$test_mode" -eq 1 ] && target_root="/tmp/$USER"
+
+if [ -d "$target_root/.ssh" ]; then
+  chmod 700 "$target_root/.ssh"
+  find "$target_root/.ssh/" -type f -exec chmod 600 {} \;
+fi
+
+if [ -d "$target_root/.gnupg" ]; then
+  chmod 700 "$target_root/.gnupg"
+  find "$target_root/.gnupg/" -type d -exec chmod 700 {} \;
+  find "$target_root/.gnupg/" -type f -exec chmod 600 {} \;
+fi
